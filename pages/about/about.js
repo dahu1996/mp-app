b0a23a6f50893efc8e3191b42ce549583bd38c05
lishuhuang <lishuhang@kingyee.com.cn>
2021-12-19 15:53:10 +0800

    练习

--- pages/about/about.js
+++
@@ -1,9 +0,0 @@
-Page({
-  onRequest() {
-    wx.cloud.callFunction({
-      name: 'hello'
-    })
-    .then(console.log)
-    .catch(console.error)
-  }
-});
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,29 +1,28 @@
 Page({
   data: {
-    value: 'foo',
-    img: 'https://th.bing.com/th/id/R.c5091f0a67c7ff712fc81f9296468764?rik=bT%2fUeCjG1E06yg&riu=http%3a%2f%2fpic.616pic.com%2fys_bnew_img%2f00%2f17%2f66%2fTE2BbvrCFC.jpg&ehk=x%2bZaNC9Xfw0y9fRReIcoy18IWT92Q%2bhpuRHo8oEK4D4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-    count: 0,
-    flag: false
+    swiperList: [],
+    gridList: [],
   },
-  clickMe(event) {
-    this.setData({
-      count: this.data.count + 1
+  onLoad() {
+    this.getSwiperList();
+    this.getGridList();
+  },
+  getSwiperList() {
+    wx.request({
+      url: 'https://www.escook.cn/api/api/slides',
+      method: 'GET',
+      success: ({ data }) => {
+        this.setData({ 'swiperList': data });
+      }
     })
-    console.log(event);
   },
-  onInupt(event) {
-    this.setData({
-      count: event.detail.value
-    });
-  },
-  onPullDownRefresh() {
-    setTimeout(() => {
-      wx.stopPullDownRefresh({
-        success: (res) => {console.log(res)},
-      })
-    }, 1000);
-  },
-  onReachBottom() {
-    console.log('reach bottom');
+  getGridList() {
+    wx.request({
+      url: 'https://www.escook.cn/categories',
+      method: 'GET',
+      success: ({ data }) => {
+        this.setData({ 'gridList': data });
+      }
+    })
   }
 })
